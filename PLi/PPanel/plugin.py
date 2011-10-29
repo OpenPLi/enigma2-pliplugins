7f24d14b69705448cabf69ba0aeb561e37be2931
MiLo <milo-software@users.sourceforge.net>
2011-10-29 14:04:04 +0200

    Remove plugins that were moved to github/E2OpenPlugins

--- PLi/PPanel/plugin.py
+++
@@ -1,8 +0,0 @@
-from Plugins.Plugin import PluginDescriptor
-
-def main(session, **kwargs):
-	from ppanel import ToplevelPPanel
-	session.open(ToplevelPPanel)
-
-def Plugins(**kwargs):
-	return PluginDescriptor(name = "PPanels", description = "Lets you execute your PPanels", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc = main)
